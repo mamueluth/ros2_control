c54ff02d85a872ec6171b4e2e54c67680d4a33c7
Manuel M <mamueluth@gmail.com>
2022-11-14 08:23:00 +0100

    rename include guard

--- controller_manager/include/controller_manager/distributed_control/state_publisher.hpp
+++ controller_manager/include/controller_manager/distributed_control/state_publisher.hpp
@@ -1,13 +1,15 @@
-#ifndef DISTRIBUTED_CONTROL__STATE_PUBLISHER
-#define DISTRIBUTED_CONTROL__STATE_PUBLISHER
+#ifndef DISTRIBUTED_CONTROL__STATE_PUBLISHER_HPP_
+#define DISTRIBUTED_CONTROL__STATE_PUBLISHER_HPP_
 
 #include <memory>
 
-#include "hardware_interface/loaned_state_interface.hpp"
 #include "visibility_control.h"
+
+#include "hardware_interface/loaned_state_interface.hpp"
 
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_lifecycle/lifecycle_node.hpp"
+#include "rclcpp_lifecycle/state.hpp"
 #include "std_msgs/msg/float64.hpp"
 
 namespace distributed_control
@@ -22,17 +24,17 @@ public:
     
     STATE_PUBLISHER_PUBLIC
     std::shared_ptr<rclcpp_lifecycle::LifecycleNode> get_node();
-    
+
 private:
     void on_timer();
 
     const std::string & namespace_;
-    std::shared_ptr<rclcpp_lifecycle::LifecycleNode> node_;
     std::unique_ptr<hardware_interface::LoanedStateInterface> loaned_state_interface_ptr_;
+    std::shared_ptr<rclcpp_lifecycle::LifecycleNode> node_;
     rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr state_value_pub_;
     rclcpp::TimerBase::SharedPtr timer_;
 };
 
 } // namespace distributed_control
 
-#endif // DISTRIBUTED_CONTROL__STATE_PUBLISHER
+#endif // DISTRIBUTED_CONTROL__STATE_PUBLISHER_HPP_
--- controller_manager/src/distributed_control/state_publisher.cpp
+++ controller_manager/src/distributed_control/state_publisher.cpp
@@ -12,18 +12,18 @@ namespace distributed_control
 StatePublisher::StatePublisher(const std::string & ns, std::unique_ptr<hardware_interface::LoanedStateInterface> loaned_state_interface_ptr)
 : namespace_(ns), loaned_state_interface_ptr_(std::move(loaned_state_interface_ptr))
 {
-    rclcpp::NodeOptions node_options;
-    node_ = std::make_shared<rclcpp_lifecycle::LifecycleNode>(loaned_state_interface_ptr_->get_underscore_separated_name() + "_node", namespace_, node_options, false);
-    RCLCPP_INFO(node_->get_logger(), "Creating StatePublisher<%s>.", loaned_state_interface_ptr_->get_underscore_separated_name().c_str());
-    state_value_pub_ = node_->create_publisher<std_msgs::msg::Float64>(loaned_state_interface_ptr_->get_underscore_separated_name(), 10);
-    timer_ = node_->create_wall_timer(1s, std::bind(&StatePublisher::on_timer, this));
+  rclcpp::NodeOptions node_options;
+  node_ = std::make_shared<rclcpp_lifecycle::LifecycleNode>(loaned_state_interface_ptr_->get_underscore_separated_name() + "_node", namespace_, node_options, false);
+  state_value_pub_ = node_->create_publisher<std_msgs::msg::Float64>(loaned_state_interface_ptr_->get_underscore_separated_name(), 10);
+  timer_ = node_->create_wall_timer(1ms, std::bind(&StatePublisher::on_timer, this));
+  RCLCPP_INFO(node_->get_logger(), "Creating StatePublisher<%s>.", loaned_state_interface_ptr_->get_underscore_separated_name().c_str());
 }
 
 std::shared_ptr<rclcpp_lifecycle::LifecycleNode> StatePublisher::get_node() 
 {
   if (!node_.get())
   {
-    throw std::runtime_error("Lifecycle node hasn't been initialized yet!");
+    throw std::runtime_error("Lifecycle node hasn't been configured yet!");
   }
   return node_;
 }
